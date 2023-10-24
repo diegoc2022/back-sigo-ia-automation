96810ba05a3e69f29d7a99ea4104623520d8ef7c
diegoc2022 <diegocastanoaj2022@gmail.com>
2023-10-24 17:54:45 -0500

    Actualizar conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,12 @@ import { Usuario } from './login/entity/user';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host:'dbcmtest.loc',
-      port:5432,
-      username:'dacastanos',
-      password:'QFcc?;[F0S8M',
-      database:'cm',
+      type:'postgres',
+      host:process.env.POSTGRES_HOST,
+      port:parseInt(process.env.POSTGRES_PORT),
+      username:process.env.POSTGRES_USER,
+      password:process.env.POSTGRES_PASSWORD,
+      database:process.env.POSTGRES_DATABASE,
       entities: [Usuario],
       synchronize: true,    
   }),  
